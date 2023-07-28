115b1b63c87c1e278d67cee8596d06022b6c6192
Thanh Doan <doan.teddy@gmail.com>
2023-07-27 21:33:32 -0400

    fixed seed files

--- server/models/Vendor.js
+++ server/models/Vendor.js
@@ -12,11 +12,6 @@ const bcrypt = require('bcrypt');
           type: String, 
           required: true,
         },
-        username: {
-          type: String,
-          required: true,
-          unique: true,
-        },
         email: {
           type: String,
           required: true,
--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,15 +1,15 @@
-// const db = require('../config/connection');
-// const { Profile } = require('../models');
-// const profileSeeds = require('./profileSeeds.json');
+const db = require('../config/connection');
+const { Vendor } = require('../models');
+const vendorSeeds = require('./vendorSeeds.json');
 
-// db.once('open', async () => {
-//   try {
-//     await Profile.deleteMany({});
-//     await Profile.create(profileSeeds);
+db.once('open', async () => {
+  try {
+    await Vendor.deleteMany({});
+    await Vendor.create(vendorSeeds);
 
-//     console.log('all done!');
-//     process.exit(0);
-//   } catch (err) {
-//     throw err;
-//   }
-// });
+    console.log('all done!');
+    process.exit(0);
+  } catch (err) {
+    throw err;
+  }
+});
