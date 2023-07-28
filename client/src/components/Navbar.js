ebc30ead81b0181206a060931b762aba89a4788a
EFP18 <ester.p18@hotmail.com>
2023-07-27 21:05:08 -0500

    onclick

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -25,138 +25,139 @@ function Navbar() {
     <div>
       {/* {Auth.loggedIn() ? (
         <> */}
-          <Box
-            sx={{
-              position: 'fixed',
-              top: 0,
-              left: 0,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              height: '100vh',
-              width: isHovered ? '200px' : '50px',
-              padding: '20px',
-              backgroundColor: colors.black,
-              transition: 'width 0.3s',
-              zIndex: 1,
-            }}
-            onMouseEnter={handleHover(true)}
-            onMouseLeave={handleHover(false)}
-          >
-            <Typography
-              variant='h4'
-              style={{ color: colors.primary, marginBottom: '20px' }}
+      <Box
+        sx={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          height: '100vh',
+          width: isHovered ? '200px' : '50px',
+          padding: '20px',
+          backgroundColor: colors.black,
+          transition: 'width 0.3s',
+          zIndex: 1,
+        }}
+        onMouseEnter={handleHover(true)}
+        onMouseLeave={handleHover(false)}
+      >
+        <Typography
+          variant='h4'
+          style={{ color: colors.primary, marginBottom: '20px' }}
+        >
+          {isHovered ? 'appointme.' : 'am.'}
+        </Typography>
+        <IconButton style={{ pointerEvents: 'none' }}>
+          <img
+            src={editIcon}
+            alt='edit'
+            style={{ width: '24px', height: '24px', pointerEvents: 'auto' }}
+          />
+          {isHovered && (
+            <Link
+              to='/vendorprofile'
+              style={{
+                color: colors.primary,
+                textDecoration: 'none',
+                marginLeft: '10px',
+                pointerEvents: 'auto',
+              }}
             >
-              {isHovered ? 'appointme.' : 'am.'}
-            </Typography>
-            <IconButton style={{ pointerEvents: 'none' }}>
-              <img
-                src={editIcon}
-                alt='edit'
-                style={{ width: '24px', height: '24px', pointerEvents: 'auto' }}
-              />
-              {isHovered && (
-                <Link
-                  to='/vendorprofile'
-                  style={{
-                    color: colors.primary,
-                    textDecoration: 'none',
-                    marginLeft: '10px',
-                    pointerEvents: 'auto',
-                  }}
-                >
-                  Edit Profile
-                </Link>
-              )}
-            </IconButton>
-            <IconButton style={{ pointerEvents: 'none' }}>
-              <img
-                src={viewIcon}
-                alt='view'
-                style={{ width: '24px', height: '24px', pointerEvents: 'auto' }}
-              />
-              {isHovered && (
-                <Link
-                  to='/profileview'
-                  style={{
-                    color: colors.primary,
-                    textDecoration: 'none',
-                    marginLeft: '10px',
-                    pointerEvents: 'auto',
-                  }}
-                >
-                  Profile View
-                </Link>
-              )}
-            </IconButton>
-            <IconButton style={{ pointerEvents: 'none' }}>
-              <img
-                src={calendarIcon}
-                alt='calendar'
-                style={{ width: '24px', height: '24px', pointerEvents: 'auto' }}
-              />
-              {isHovered && (
-                <Link
-                  to='/calendarpage'
-                  style={{
-                    color: colors.primary,
-                    textDecoration: 'none',
-                    marginLeft: '10px',
-                    pointerEvents: 'auto',
-                  }}
-                >
-                  Calendar
-                </Link>
-              )}
-            </IconButton>
-            <IconButton style={{ pointerEvents: 'none' }}>
-              <img
-                src={clientsIcon}
-                alt='clients'
-                style={{ width: '24px', height: '24px', pointerEvents: 'auto' }}
-              />
-              {isHovered && (
-                <Link
-                  to='/clientDb'
-                  style={{
-                    color: colors.primary,
-                    textDecoration: 'none',
-                    marginLeft: '10px',
-                    pointerEvents: 'auto',
-                  }}
-                >
-                  Client's
-                </Link>
-              )}
-            </IconButton>
-            <Box sx={{ mt: 'auto' }}>
-              <IconButton style={{ pointerEvents: 'none' }}>
-                <img
-                  src={logoutIcon}
-                  alt='logout'
-                  style={{
-                    width: '24px',
-                    height: '24px',
-                    marginRight: '10px',
-                    pointerEvents: 'auto',
-                  }}
-                />
-                {isHovered && (
-                  <Typography
-                    style={{
-                      pointerEvents: 'auto',
-                      color: colors.primary,
-                      textDecoration: 'none',
-                      marginLeft: '10px',
-                    }}
-                  >
-                    Sign Out
-                  </Typography>
-                )}
-              </IconButton>
-            </Box>
-          </Box>
-        {/* </>
+              Edit Profile
+            </Link>
+          )}
+        </IconButton>
+        <IconButton style={{ pointerEvents: 'none' }}>
+          <img
+            src={viewIcon}
+            alt='view'
+            style={{ width: '24px', height: '24px', pointerEvents: 'auto' }}
+          />
+          {isHovered && (
+            <Link
+              to='/profileview'
+              style={{
+                color: colors.primary,
+                textDecoration: 'none',
+                marginLeft: '10px',
+                pointerEvents: 'auto',
+              }}
+            >
+              Profile View
+            </Link>
+          )}
+        </IconButton>
+        <IconButton style={{ pointerEvents: 'none' }}>
+          <img
+            src={calendarIcon}
+            alt='calendar'
+            style={{ width: '24px', height: '24px', pointerEvents: 'auto' }}
+          />
+          {isHovered && (
+            <Link
+              to='/calendarpage'
+              style={{
+                color: colors.primary,
+                textDecoration: 'none',
+                marginLeft: '10px',
+                pointerEvents: 'auto',
+              }}
+            >
+              Calendar
+            </Link>
+          )}
+        </IconButton>
+        <IconButton style={{ pointerEvents: 'none' }}>
+          <img
+            src={clientsIcon}
+            alt='clients'
+            style={{ width: '24px', height: '24px', pointerEvents: 'auto' }}
+          />
+          {isHovered && (
+            <Link
+              to='/clientDb'
+              style={{
+                color: colors.primary,
+                textDecoration: 'none',
+                marginLeft: '10px',
+                pointerEvents: 'auto',
+              }}
+            >
+              Client's
+            </Link>
+          )}
+        </IconButton>
+        <Box sx={{ mt: 'auto' }}>
+          <IconButton style={{ pointerEvents: 'none' }}>
+            <img
+              src={logoutIcon}
+              alt='logout'
+              style={{
+                width: '24px',
+                height: '24px',
+                marginRight: '10px',
+                pointerEvents: 'auto',
+              }}
+            />
+            {isHovered && (
+              <Typography
+                style={{
+                  pointerEvents: 'auto',
+                  color: colors.primary,
+                  textDecoration: 'none',
+                  marginLeft: '10px',
+                }}
+                onClick={logout}
+              >
+                Sign Out
+              </Typography>
+            )}
+          </IconButton>
+        </Box>
+      </Box>
+      {/* </>
       ) : (
         <></>
       )} */}
