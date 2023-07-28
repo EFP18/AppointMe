779def199c40cc4cdc133135ca2ba0a99746a1b6
Drew Smith <drgh056@gmail.com>
2023-07-27 20:50:31 -0500

    Created typedefs and moved incorrect script in typeDefs to index.js in schemas folder

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,58 @@
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+const { gql } = require('apollo-server-express');
 
-module.exports = { typeDefs, resolvers };
+const typeDefs = gql`
+    type Business {
+        _id: ID
+        name: String
+        description: String
+        logo: String
+        image: String
+        address: String
+        phone: String
+        email: String
+        socialMedia: SocialMedia
+        tags: [Tag]
+        services: [Service]
+        clients: [Client]
+    }
+
+    type Client {
+        _id: ID
+        firstName: String
+        lastName: String
+        email: String
+        address: String
+        phone: String
+        note: String
+        previousShopping: [Business]
+    }
+
+    type Service {
+        name: String
+        description: String
+        price: Float
+    }
+
+    type SocialMedia {
+        facebook: String
+        instagram: String
+        youTube: String
+        tikTok: String
+        linkedIn: String
+    }
+
+    type Tag {
+        _id: ID
+        name: String
+    }
+
+    type Vendor {
+        _id: ID
+        firstName: String
+        lastName: String
+        email: String
+        password: String
+        business: Business
+    }
+`
+module.exports = typeDefs;
